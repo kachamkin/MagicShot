d31c79b022b84af15a5b49446e9ce52c81211c6a
kachamkin <kachamkin@gmail.com>
2022-12-18 15:54:43 +0200

    Resize

--- Cards/header.h
+++ Cards/header.h
@@ -12,8 +12,8 @@
 using namespace std;
 
 #define EVENT_TIMEOUT 64
-#define BORDER_WIDTH 20
-#define BORDER_COLOR 0, 0, 255, 0
+#define BORDER_WIDTH 40
+#define BORDER_COLOR 80, 80, 80, 0
 #define SELECTION_COLOR 255, 0, 0, 0
 #define SELECTION_WIDTH 3
 #define WINDOW_OPACITY 0.3
